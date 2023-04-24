71352d7b4cef49aa44e7dcd90dcb48d8cffa52fb
SolRubinetti <mariasolrubinetti@gmail.com>
2023-04-24 16:56:54 -0300

    Ejercicio 2-6

---
+++ rubinetti_maria_sol_1d_clase_2_ejercicio_2_61.ino
@@ -0,0 +1,82 @@
+// Nombre: Rubinetti Maria Sol
+// Ejercicio: 2-6
+//C++ code
+//
+void setup()
+{
+  pinMode(13, OUTPUT);
+  pinMode(12, OUTPUT);
+  pinMode(11, OUTPUT);
+  pinMode(10, OUTPUT);
+  pinMode(9, OUTPUT);
+  pinMode(8, OUTPUT);
+  
+}
+
+void loop()
+{
+  digitalWrite(13, HIGH);
+  delay(200); // Wait for 500 millisecond(s)
+  digitalWrite(13, LOW);
+  delay(200); // Wait for 500 millisecond(s)
+  digitalWrite(12, HIGH);
+  delay(200); // Wait for 500 millisecond(s)
+  digitalWrite(12, LOW);
+  delay(200); // Wait for 500 millisecond(s)
+  digitalWrite(11, HIGH);
+  delay(200); // Wait for 500 millisecond(s)
+  digitalWrite(11, LOW);
+  delay(200); // Wait for 500 millisecond(s)
+  digitalWrite(10, HIGH);
+  delay(200); // Wait for 500 millisecond(s)
+  digitalWrite(10, LOW);
+  delay(200); // Wait for 500 millisecond(s)
+  digitalWrite(9, HIGH);
+  delay(200); // Wait for 500 millisecond(s)
+  digitalWrite(9, LOW);
+  delay(200); // Wait for 500 millisecond(s)
+  digitalWrite(8, HIGH);
+  delay(200); // Wait for 500 millisecond(s)
+  digitalWrite(8, LOW);
+  digitalWrite(13, HIGH);
+  delay(200); // Wait for 500 millisecond(s)
+  digitalWrite(13, LOW);
+  delay(200); // Wait for 500 millisecond(s)
+  digitalWrite(12, HIGH);
+  delay(200); // Wait for 500 millisecond(s)
+  digitalWrite(12, LOW);
+  delay(200); // Wait for 500 millisecond(s)
+  digitalWrite(11, HIGH);
+  delay(200); // Wait for 500 millisecond(s)
+  digitalWrite(11, LOW);
+  delay(200); // Wait for 500 millisecond(s)
+  digitalWrite(10, HIGH);
+  delay(200); // Wait for 500 millisecond(s)
+  digitalWrite(10, LOW);
+  delay(200); // Wait for 500 millisecond(s)
+  digitalWrite(9, HIGH);
+  delay(200); // Wait for 500 millisecond(s)
+  digitalWrite(9, LOW);
+  delay(200); // Wait for 500 millisecond(s)
+  digitalWrite(8, HIGH);
+  delay(200); // Wait for 500 millisecond(s)
+  digitalWrite(8, LOW);
+  digitalWrite(9, HIGH);
+  delay(200); // Wait for 500 millisecond(s)
+  digitalWrite(9, LOW);
+  delay(200); // Wait for 500 millisecond(s)
+  digitalWrite(10, HIGH);
+  delay(200); // Wait for 500 millisecond(s)
+  digitalWrite(10, LOW);
+  delay(200); // Wait for 500 millisecond(s)
+  digitalWrite(11, HIGH);
+  delay(200); // Wait for 500 millisecond(s)
+  digitalWrite(11, LOW);
+  delay(200); // Wait for 500 millisecond(s)
+  digitalWrite(12, HIGH);
+  delay(200); // Wait for 500 millisecond(s)
+  digitalWrite(12, LOW);
+  delay(200); // Wait for 500 millisecond(s)
+
+
+}
